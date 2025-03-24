36bd9d45a3c3de76491619f68f4d845d0f09e5b2
SonSeHyeok <sonsh7041@naver.com>
2025-03-24 19:28:40 +0900

    이름과 나이 출력

---
+++ java_practice/Practice_4-1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "손세혁";
+        int age = 26;
+        System.out.println(name + "님은 " + age + "살입니다");
+    }
+}
